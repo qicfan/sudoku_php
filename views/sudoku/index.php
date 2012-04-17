b2efe2e038931d5056ec13085acc9847b2db08b0
zeroq <qicfan@gmail.com>
2012-04-17 23:19:03 +0800

    edit html

--- views/sudoku/index.php
+++ views/sudoku/index.php
@@ -1,4 +1,8 @@
-<table width="200" border="1" cellpadding="0" cellspacing="0">
+<style>
+body {font-size:18px;}
+.have_zero input {width:30px; height:30px; text-align:center; font-size:16px;}
+</style>
+<table width="500" height="420" border="1" cellpadding="0" cellspacing="0">
 <?php
 for ($y=0;$y<9;$y++):
 ?>
@@ -6,7 +10,7 @@ for ($y=0;$y<9;$y++):
 <?php
 for ($x=0;$x<9;$x++):
 ?>
-		<td id="td_<?php echo $y . '_' . $x;?>" class="<?php echo $sudoku[$y][$x] > 0 ? 'no_zero' : 'have_zero'?>"><?php echo $sudoku[$y][$x] > 0 ? $sudoku[$y][$x] : '&nbsp;' ?></td>
+		<td id="td_<?php echo $y . '_' . $x;?>" class="<?php echo $sudoku[$y][$x] > 0 ? 'no_zero' : 'have_zero'?>"><?php if ($sudoku[$y][$x] > 0): ?><span><?php echo $sudoku[$y][$x] ?></span> <?php else: ?> <input type='text' id='<?php echo $y . '_' . $x; ?>' name='sudoku_input' /><?php endif; ?></td>
 <?php
 endfor;
 ?>
@@ -15,3 +19,4 @@ endfor;
 endfor;
 ?>
 </table>
+
