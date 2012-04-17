e6f2c22005712f9e6848fed62f9fd5b820c2d25f
zeroq <qicfan@gmail.com>
2012-04-17 23:17:19 +0800

    init

--- views/sudoku/index.php
+++ views/sudoku/index.php
@@ -1,12 +1,12 @@
-<table>
+<table width="200" border="1" cellpadding="0" cellspacing="0">
 <?php
 for ($y=0;$y<9;$y++):
 ?>
-	<tr>
+	<tr valign="middle" align="center">
 <?php
 for ($x=0;$x<9;$x++):
 ?>
-		<td><?php echo $sudoku[$y][$x] > 0 ? $sudoku : '&nbsp;' ?></td>
+		<td><?php echo $sudoku[$y][$x] > 0 ? $sudoku[$y][$x] : '&nbsp;' ?></td>
 <?php
 endfor;
 ?>
